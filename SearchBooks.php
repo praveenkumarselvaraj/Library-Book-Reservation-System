a69af5d9a33ba40d15a3d7760d8314ba26477f6b
praveenkumarselvaraj <34643070+praveenkumarselvaraj@users.noreply.github.com>
2019-07-29 20:40:30 +0530

    Add files via upload

---
+++ SearchBooks.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html>
+    <link rel="stylesheet" href="style1.css">
+<body background="oldlibrarydetail.jpg">
+<a href="login.php"><img src="11949891272097570686aiga_left_arrow1.svg.hi.png" class="back"></a>
+<form  class="box"action = "DisplayBooks.php" method="get">
+    <div class="mazz"><h1>Enter the title of the books</h1></div>
+    <div class="input-group"><center><input type="text" name="search" size="50"></center></div>
+<br>
+<center><input type="submit" value="submit"></center>
+    
+        
+<br>
+  
+</form>
+</body>
+</html>
