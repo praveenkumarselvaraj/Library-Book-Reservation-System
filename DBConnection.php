cdcf458447c32fa4b14f0a23163f331b3a2077e3
praveenkumarselvaraj <34643070+praveenkumarselvaraj@users.noreply.github.com>
2019-07-29 20:31:28 +0530

    Add files via upload

---
+++ DBConnection.php
@@ -0,0 +1,10 @@
+
+<?php
+define('DB_SERVER', 'localhost:3306');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'registration'); //where books is the database name
+$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+ 
+?>
+<br>
