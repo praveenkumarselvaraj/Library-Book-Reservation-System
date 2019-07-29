5bf57514d8ffb06bc1aa774b858b2f82556dd152
praveenkumarselvaraj <34643070+praveenkumarselvaraj@users.noreply.github.com>
2019-07-29 20:38:23 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,31 @@
+<?php include('server.php') ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Login</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <a href="list.html"><img src="11949891272097570686aiga_left_arrow1.svg.hi.png" class="back"></a>
+  <div class="header">
+      <a><img src="user.png" class="back1"></a>
+  	
+  </div>
+	 
+  <form method="post" action="login.php">
+  	<?php include('errors.php'); ?>
+  	<div class="input-group">
+  		<label>Username</label>
+  		<input type="text" name="username" >
+  	</div>
+  	<div class="input-group">
+  		<label>Password</label>
+  		<input type="password" name="password">
+  	</div>
+  	<div class="input-group">
+  		<button type="submit" class="btn" name="login_user">Login</button>
+  	</div>
+  
+  </form>
+</body>
+</html>
